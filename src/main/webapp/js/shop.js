0a8d0a736b7efdb49cff3975c20705deb11c72ec
whwog <whwog@192.168.100.150>
2024-06-02 20:22:59 +0900

    shopMain 디자인 변경

--- src/main/webapp/js/shop.js
+++ src/main/webapp/js/shop.js
@@ -1,83 +1,170 @@
+///*
+//	Add to cart fly effect with jQuery. - May 05, 2013
+//	(c) 2013 @ElmahdiMahmoud - fikra-masri.by
+//	license: https://www.opensource.org/licenses/mit-license.php
+//*/   
+//$(function(){
+//	$('.add-to-cart').on('click', function () {
+//        var cart = $('.shopping-cart');
+//        var imgtodrag = $(this).parent('.item').find("img").eq(0);
+//        let productId = $(this).parent().attr('data-product-id');
+//        
+//        console.log("productId" , productId);
+//        
+//        
+//        
+//      console.log(1);
+//      insertItem(productId).then(check =>{
+//      if(check){
+//	
+//            if (imgtodrag) {
+//                var imgclone = imgtodrag.clone()
+//                    .offset({
+//                    top: imgtodrag.offset().top,
+//                    left: imgtodrag.offset().left
+//                })
+//                  .css({
+//                  'opacity': '0.5',
+//                      'position': 'absolute',
+//                      'height': '150px',
+//                      'width': '150px',
+//                      'z-index': '100'
+//                })
+//                  .appendTo($('body'))
+//                  
+//                  .animate({
+//                  'top': cart.offset().top + 10,
+//                      'left': cart.offset().left + 10,
+//                      'width': 75,
+//                      'height': 75
+//                }, 700);
+//
+//                imgclone.animate({
+//                    'width': 0,
+//                        'height': 0
+//                }, function () {
+//                    $(this).detach()
+//                });
+//              }
+//        }
+//      });
+//	});
+//});
+//	
+//
+//const insertItem = async(productId) =>{
+//	let check = false;
+//	
+//	try{
+//		await  $.ajax({
+//            url: '/shopping/Cartupdate.do', // Controller의 URL로 변경하세요.
+//            type: 'POST', // 또는 'GET', 서버에서 요구하는 방식에 맞춰서 설정하세요.
+//            data: {
+//                productId: productId // 서버로 전송할 데이터
+//            },
+//          	success: function(response) {
+//		        if (response.success) {
+//		          check = true;
+//		        } else {
+//		          alert('Failed to remove product');
+//		        }
+//            },
+//            error: function(xhr, status, error) {
+//                // 요청 실패 시 실행될 코드
+//                console.error(error); // 오류 메시지 출력
+//            }
+//        });
+//		
+//	}catch(error){
+//    	console.error('There was an error!', error);
+//	}
+//	
+//	return check;
+//}
+
 /*
-	Add to cart fly effect with jQuery. - May 05, 2013
-	(c) 2013 @ElmahdiMahmoud - fikra-masri.by
-	license: https://www.opensource.org/licenses/mit-license.php
+    Add to cart fly effect with jQuery. - May 05, 2013
+    (c) 2013 @ElmahdiMahmoud - fikra-masri.by
+    license: https://www.opensource.org/licenses/mit-license.php
 */   
-$(function(){
-	$('.add-to-cart').on('click', function () {
-        var cart = $('.shopping-cart');
-        var imgtodrag = $(this).parent('.item').find("img").eq(0);
-        let productId = $(this).parent().attr('data-product-id');
-        
-        console.log("productId" , productId);
-        
-        
-        
-      console.log(1);
-      insertItem(productId).then(check =>{
+$(function() {
+    $('.add-to-cart').on('click', function () {
+        var cart = $('.shopping-cart'); // 쇼핑 카트 아이콘의 선택자
+        var imgtodrag = $(this).closest('.item').find("img").eq(0); // 현재 아이템의 이미지를 찾습니다.
+        let productId = $(this).closest('.item').attr('data-product-id'); // 제품 ID를 가져옵니다.
+
+        console.log("productId", productId);
+		
+		insertItem(productId).then(check =>{
       if(check){
-	
-            if (imgtodrag) {
-                var imgclone = imgtodrag.clone()
-                    .offset({
-                    top: imgtodrag.offset().top,
-                    left: imgtodrag.offset().left
-                })
-                  .css({
-                  'opacity': '0.5',
-                      'position': 'absolute',
-                      'height': '150px',
-                      'width': '150px',
-                      'z-index': '100'
-                })
-                  .appendTo($('body'))
-                  
-                  .animate({
-                  'top': cart.offset().top + 10,
-                      'left': cart.offset().left + 10,
-                      'width': 75,
-                      'height': 75
-                }, 700);
+		
+        if (cart.length > 0 && imgtodrag.length > 0) {
+            var cartOffset = cart.offset(); // 카트 아이콘의 위치를 계산합니다.
+            var imgtodragOffset = imgtodrag.offset(); // 이미지의 위치를 계산합니다.
 
-                imgclone.animate({
-                    'width': 0,
-                        'height': 0
-                }, function () {
-                    $(this).detach()
+            var imgclone = imgtodrag.clone()
+                .css({
+                    'opacity': '0.5',
+                    'position': 'absolute',
+                    'height': '150px',
+                    'width': '150px',
+                    'z-index': '100',
+                    'top': imgtodragOffset.top,
+                    'left': imgtodragOffset.left
+                })
+                .appendTo($('body'))
+                .animate({
+                    'top': cartOffset.top + 10,
+                    'left': cartOffset.left + 10,
+                    'width': 75,
+                    'height': 75
+                }, 700, 'swing', function () { // 기본 제공 easing 함수 사용
+                    $(this).detach();
                 });
-              }
-        }
-      });
-	});
-});
-	
 
-const insertItem = async(productId) =>{
-	let check = false;
-	
-	try{
-		await  $.ajax({
+            imgclone.animate({
+                'width': 0,
+                'height': 0
+            }, function () {
+                $(this).detach();
+            });
+        } else {
+            alert('Failed to locate cart icon or image element');
+        }
+        
+       	}
+		}); 
+    });
+});
+
+const insertItem = async (productId) => {
+    let check = false;
+    
+    try {
+        await $.ajax({
             url: '/shopping/Cartupdate.do', // Controller의 URL로 변경하세요.
             type: 'POST', // 또는 'GET', 서버에서 요구하는 방식에 맞춰서 설정하세요.
             data: {
                 productId: productId // 서버로 전송할 데이터
             },
-          	success: function(response) {
-		        if (response.success) {
-		          check = true;
-		        } else {
-		          alert('Failed to remove product');
-		        }
+            success: function(response) {
+                if (response.success) {
+                    check = true;
+                } else {
+                    alert('Failed to add product to cart');
+                    console.error('Response Error:', response); // 응답 오류 출력
+                }
             },
             error: function(xhr, status, error) {
-                // 요청 실패 시 실행될 코드
-                console.error(error); // 오류 메시지 출력
+                console.error('Request Error:', error); // 요청 실패 시 오류 메시지 출력
+                console.error('Status:', status); // 상태 코드 출력
+                console.error('Response:', xhr.responseText); // 서버 응답 출력
+                alert('An error occurred while adding the product to the cart');
             }
         });
-		
-	}catch(error){
-    	console.error('There was an error!', error);
-	}
-	
-	return check;
+    } catch(error) {
+        console.error('There was an error!', error);
+    }
+    
+    return check;
 }
--- src/main/webapp/shopping/shopMain.jsp
+++ src/main/webapp/shopping/shopMain.jsp
@@ -1,3 +1,67 @@
+<%-- <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%> --%>
+<%-- <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %> --%>
+
+<!-- <!DOCTYPE html> -->
+<!-- <html> -->
+<!-- <head> -->
+<!-- <meta charset="UTF-8"> -->
+<!-- <title>page</title> -->
+<!-- <link rel="stylesheet" type="text/css" href="../css/shop.css"> -->
+
+<!-- <script src="http://code.jquery.com/jquery-3.5.1.min.js"></script> -->
+<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
+<!-- <script src="../js/shop.js"></script> -->
+<!-- <script src="../js/page.js"></script> -->
+<!-- </head> -->
+
+<!-- <!-- <body> --> -->
+
+<!--  <div class="container"> -->
+<!--     <div id="modal_menubar"> -->
+<!--       <div id="modalContent_menubar"></div> -->
+<!--     </div> -->
+  
+  
+<!-- 	<div class = "shopCloumn"> -->
+<!-- 		<!-- wrapper --> -->
+<!-- 		<div class="wrapper"> -->
+		
+<!-- 		<div class="wiki"> -->
+<!-- 			<input type="search" name="search" id="search" placeholder="search"> -->
+<!-- 			<button class="search">Search</button> -->
+<!-- 		</div> -->
+<!-- 		<div class="outcome"></div> -->
+<!-- 			<!-- <h1>Bike Stock</h1> -->
+<!-- 	<!-- 	 	<span ><a href="cartMain.jsp"><i class="shopping-cart"> </i></a></span> --> -->
+		
+<!-- 			<div class="clear"></div> -->
+<!-- 			<!-- items -->
+<!-- 			<div class="items"> -->
+<%-- 		        <c:choose> --%>
+<%-- 		          <c:when test="${empty products}"> --%>
+<!-- 		            <h2>없음</h2> -->
+<%-- 		          </c:when> --%>
+<%-- 					<c:otherwise> --%>
+<%--             			<c:forEach var="product" items="${products}" varStatus="loop"> --%>
+<!-- 							single item -->
+<%-- 							<div class="item" data-product-id="${product.productId}"> --%>
+<!-- 								<img src="http://img1.exportersindia.com/product_images/bc-small/dir_55/1620613/cannondale-jekyll-1-2011-mountain-bike-309779.jpg" alt="item" /> -->
+<%-- 								<h2>${product.name}</h2> --%>
+<%-- 								<p>가격: <em>${product.price}</em></p> --%>
+<!-- 								<button class="add-to-cart" type="button">Add to cart</button> -->
+<!-- 							</div> -->
+<%-- 						</c:forEach> --%>
+<%-- 					</c:otherwise> --%>
+<%-- 				</c:choose> --%>
+<!-- 			</div> -->
+<!-- 			<!--/ items -->
+<!-- 		</div> -->
+<!-- 		<!--/ wrapper --> 
+<!-- 	</div> -->
+<!-- </div> -->
+<!-- </body> -->
+<!-- </html> -->
+
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 
@@ -6,58 +70,59 @@
 <head>
 <meta charset="UTF-8">
 <title>page</title>
+<!-- 부트스트랩 CSS 포함 -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="../css/shop.css">
 
-<script src="http://code.jquery.com/jquery-3.5.1.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<!-- jQuery와 부트스트랩 JS 포함 -->
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script> <!-- jQuery UI 추가 -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 <script src="../js/shop.js"></script>
 <script src="../js/page.js"></script>
 </head>
 
-<!-- <body> -->
-
- <div class="container">
-    <div id="modal_menubar">
-      <div id="modalContent_menubar"></div>
+<body>
+<div class="container mt-5">
+    <div id="modal_menubar" class="fixed-top">
+        <div id="modalContent_menubar"></div>
     </div>
   
-  
-	<div class = "shopCloumn">
-		<!-- wrapper -->
-		<div class="wrapper">
-		
-		<div class="wiki">
-			<input type="search" name="search" id="search" placeholder="search">
-			<button class="search">Search</button>
-		</div>
-		<div class="outcome"></div>
-			<!-- <h1>Bike Stock</h1> -->
-	<!-- 	 	<span ><a href="cartMain.jsp"><i class="shopping-cart"> </i></a></span> -->
-		
-			<div class="clear"></div>
-			<!-- items -->
-			<div class="items">
-		        <c:choose>
-		          <c:when test="${empty products}">
-		            <h2>없음</h2>
-		          </c:when>
-					<c:otherwise>
-            			<c:forEach var="product" items="${products}" varStatus="loop">
-							<!-- single item -->
-							<div class="item" data-product-id="${product.productId}">
-								<img src="http://img1.exportersindia.com/product_images/bc-small/dir_55/1620613/cannondale-jekyll-1-2011-mountain-bike-309779.jpg" alt="item" />
-								<h2>${product.name}</h2>
-								<p>가격: <em>${product.price}</em></p>
-								<button class="add-to-cart" type="button">Add to cart</button>
-							</div>
-						</c:forEach>
-					</c:otherwise>
-				</c:choose>
-			</div>
-			<!--/ items -->
-		</div>
-		<!--/ wrapper -->
-	</div>
+    <div class="shopCloumn">
+        <div class="wrapper">
+            <div class="wiki mb-3">
+                <div class="input-group">
+                    <input type="search" name="search" id="search" class="form-control" placeholder="Search">
+                    <div class="input-group-append">
+                        <button class="btn btn-primary search" type="button">Search</button>
+                    </div>
+                </div>
+            </div>
+            <div class="outcome"></div>
+            
+            <div class="row">
+                <c:choose>
+                    <c:when test="${empty products}">
+                        <h2 class="text-center">없음</h2>
+                    </c:when>
+                    <c:otherwise>
+                        <c:forEach var="product" items="${products}" varStatus="loop">
+                            <div class="col-md-4 mb-4">
+                                <div class="card h-100 item" data-product-id="${product.productId}">
+                                    <img class="card-img-top" src="http://img1.exportersindia.com/product_images/bc-small/dir_55/1620613/cannondale-jekyll-1-2011-mountain-bike-309779.jpg" alt="item">
+                                    <div class="card-body">
+                                        <h5 class="card-title">${product.name}</h5>
+                                        <p class="card-text">가격: <em>${product.price}</em></p>
+                                        <button class="btn btn-success add-to-cart" type="button">Add to Cart</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </c:forEach>
+                    </c:otherwise>
+                </c:choose>
+            </div>
+        </div>
+    </div>
 </div>
 </body>
 </html>
